7d41201dad0d8817edfeebe41b5a55b74b30f60c
matias <matias.09adrian@gmail.com>
2024-03-15 22:17:48 -0300

    cambiando la alerta

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/* 
 
 let nombre = prompt("ingrese su nombre");
 let edad = Number(prompt("ingrese su edad"));
@@ -11,17 +11,17 @@ if (edad >= 18) {
 }
 
 function calcularCuotas(montoIngresado, cuotas) {
-    let cuotaMensual = montoIngresado / cuotas;
+    let cuotaMensual = montoIngresado / cuotas; //cambiar de let a const
     return cuotaMensual;
-}
+} */
 
 /* simulador de cuotas */
-function simularCuotas() {
-    let monto = prompt("Ingrese el monto total a financiar");
+/* function simularCuotas() {
+    let monto = parseFloat(prompt("Ingrese el monto a financiar"));
     let numeroMaxCuotas = parseInt(prompt("En cuantas cuotas quiere hacerlo?"));
     
-    if (isNaN(monto || numeroMaxCuotas)) { /* isnan(monto) || isnan(numeromaxcuotas) */
-        alert("Los numeros ingresados no son validos, intentelo de nuevo!");
+    if (isNaN(monto || numeroMaxCuotas)) {  /* isnan(monto) || isnan(numeromaxcuotas) */
+/*         alert("Los numeros ingresados no son validos, intentelo de nuevo!");
         return;
     } else if (monto <= 0 || numeroMaxCuotas <= 0) {
         alert("Los datos deben ser mayores a cero");
@@ -37,6 +37,48 @@ function simularCuotas() {
         resultados += "Cuotas: " + cantidadDeCuotas + " - Monto mensual: $" + cuotaMensual.toFixed(2) + "\n";
     }
     alert(resultados);
+} */
+
+// cambiar el alert por cantidad de cuotas 
+
+
+
+
+let nombre = prompt("ingrese su nombre");
+let edad = Number(prompt("ingrese su edad"));
+
+if (edad >= 18) {
+    alert("Bienvenido al simulador de cuotas " + nombre);
+    simularCuotas();
+} else {
+    alert("Tenes que ser mayor de edad para usar el simulador de cuotas!")
 }
 
+function calcularCuotas(montoIngresado, cuotas) {
+    let cuotaMensual = montoIngresado / cuotas; //cambiar de let a const
+    return cuotaMensual;
+} 
 
+/* simulador de cuotas */
+function simularCuotas() {
+    let monto = parseFloat(prompt("Ingrese el monto a financiar"));
+    let numeroMaxCuotas = parseInt(prompt("En cuantas cuotas quiere hacerlo?"));
+    
+    if (isNaN(monto || numeroMaxCuotas)) { /* isnan(monto) || isnan(numeromaxcuotas) */
+        alert("Los numeros ingresados no son validos, intentelo de nuevo!");
+        return;
+    } else if (monto <= 0 || numeroMaxCuotas <= 0) {
+        alert("Los datos deben ser mayores a cero");
+        return
+    } else {
+        alert("Procesando...")
+    }
+
+    let resultado = "Simulacion de pagos en Cuotas:\n";
+
+    for (let cantidadDeCuotas = 1; cantidadDeCuotas <= numeroMaxCuotas; cantidadDeCuotas++) {
+        let cuotaMensual = calcularCuotas(monto, cantidadDeCuotas);
+        resultado += "Cuotas: " + cantidadDeCuotas + " - Monto mensual: $" + cuotaMensual.toFixed(2) + "\n";
+    }
+    alert(cantidadDeCuotas);
+}
